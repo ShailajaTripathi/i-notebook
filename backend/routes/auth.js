c08337bd34f26e256f2eac0911209ef8259fb9bd
Shailesh <shaileshtripathi511@gmail.com>
2022-08-14 20:08:39 +0530

    express-validator on user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const router= express.Router()
 
+const { body, validationResult } = require('express-validator');
+
+
 const User= require('../models/User')
 
 //create a user using :POST "/api/auth/"
 
 
-router.post('/',(req,res)=>{
+router.post('/',[
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 })
+],(req,res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
    
-    const user = User(req.body)
-
-    user.save();
-
-    res.send(req.body)
+    User.create({
+        name: req.body.name,
+        email:req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user));
 })
 
 module.exports=router
